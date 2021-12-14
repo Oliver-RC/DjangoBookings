b59741625911e95fe42e031bb418444ca769191f
Oliver-RC <oli_cummings9@hotmail.com>
2021-12-14 20:42:29 +0000

    update admin for menu section

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Menu
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Menu)
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+    list_display = ('meal_name', 'slug', 'status', 'created_on')
+    search_fields = ['meal_name', 'description']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('meal_name',)}
+    summernote_fields = ('description',)
--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
@@ -6,12 +7,18 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Menu(models.Model):
-    meal_name = models.CharField(max_length=100)
+    meal_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)    
     description = models.TextField()
     vegetarian_vegan = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     featured_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "meal"
+        verbose_name_plural = 'meals'
 
     def __str__(self):
         return self.meal_name
