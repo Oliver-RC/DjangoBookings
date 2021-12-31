e31b95f4b927dbc1d5e4065273fd6bd7b15f6131
Oliver-RC <oli_cummings9@hotmail.com>
2021-12-31 15:35:15 +0000

    amending form view

--- bookings/views.py
+++ bookings/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from django.views import generic, View
+from django.views.generic.edit  import CreateView
 from .models import Booking
 from .forms import BookingForm
 
 
-class BookTable(generic.ListView):
+class BookTable(CreateView):
     model = Booking
     form_class = BookingForm
+    queryset = Booking.objects.all()
     template_name = "bookings.html"
 
     def reserve_table(request):
@@ -20,4 +21,5 @@ class BookTable(generic.ListView):
                 reserve_form.save()
             else:
                 reserve_form = BookingForm()
+                
         return render(request, 'bookings.html', {'form': reserve_form})
