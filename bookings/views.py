5ca6e2ca54a415fcbc7d36c1f6465c1ff00db116
Oliver-RC <oli_cummings9@hotmail.com>
2022-01-02 14:32:44 +0000

    add else statement to booking form view

--- bookings/views.py
+++ bookings/views.py
@@ -4,32 +4,11 @@ from .forms import BookingForm
 
 
 def reserve_table(request):
-    reserve_form = BookingForm()
+    reserve_form = BookingForm(data=request.POST)
 
-    if request.method == 'POST':
-        reserve_form = BookingForm(request.POST)
-
-        if reserve_form.is_valid():
-            reserve_form.save()
-            reserve_form = BookingForm()
+    if reserve_form.is_valid():
+        reserve_form.save()
+        reserve_form = BookingForm()
+    else: reserve_form = BookingForm()
 
     return render(request, 'bookings.html', {'form': reserve_form})
-
-
-# class BookTable(CreateView):
-#     model = Booking
-#     form_class = BookingForm
-#     template_name = "bookings.html"
-
-#     def reserve_table(request):
-#         reserve_form = BookingForm()
-
-#         if request.method == 'POST':
-#             reserve_form = BookingForm(request.POST)
-
-#             if reserve_form.is_valid():
-#                 reserve_form.save()
-#             else:
-#                 reserve_form = BookingForm()
-   
-#         return render(request, 'bookings.html', {'form': reserve_form})
