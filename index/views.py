daea19f37e95e0fbb45638512d3ce84ef0ab89c7
Oliver-RC <oli_cummings9@hotmail.com>
2022-01-10 19:52:44 +0000

    add content to index page

--- index/views.py
+++ index/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
+
+blogs = Post.objects.all()
 
 def index(request):
     context = {
-
+        'blogs' : blogs,
     }
 
     return render(request, 'index.html', context)
