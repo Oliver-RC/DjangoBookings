88d63afee2957212ab48a2fa4445bc52bad665bf
Oliver-RC <oli_cummings9@hotmail.com>
2022-01-14 14:34:06 +0000

    reset database

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Tables
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
 
-    list_display = ('first_name', 'last_name', 'email', 'number_of_guests', 'date', 'time', 'requirements',)
+    list_display = ('first_name', 'last_name', 'email', 'table_size', 'date', 'time', 'requirements',)
     search_fields = ['email',]
     list_filter = ('date', 'time')
     summernote_fields = ('requirements',)
+
+
+admin.site.register(Tables)
--- bookings/models.py
+++ bookings/models.py
@@ -2,27 +2,45 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-TIME_CHOICES = (
-    ('12:00', '12:00'),
-    ('13:00', '13:00'),
-    ('14:00', '14:00'),
-    ('15:00', '15:00'),
-    ('16:00', '16:00'),
-    ('17:00', '17:00'),
-    ('18:00', '18:00'),
-    ('19:00', '19:00'),
-    ('20:00', '20:00'),
-    ('21:00', '21:00'),
-)
+class Tables(models.Model):
+    TABLE_CHOICES = (
+        ('2', '2'),
+        ('4', '4'),
+        ('6', '6'),
+        ('8', '8'),
+        ('10+', '10+'),
+    )
+
+    table_size = models.CharField(max_length=200, null=True, choices=TABLE_CHOICES, default='2')
+
+    class Meta:
+        verbose_name = "table"
+        verbose_name_plural = 'tables'
+
+    def __str__(self):
+        return self.table_size
 
 
 class Booking(models.Model):
+    TIME_CHOICES = (
+        ('12:00', '12:00'),
+        ('13:00', '13:00'),
+        ('14:00', '14:00'),
+        ('15:00', '15:00'),
+        ('16:00', '16:00'),
+        ('17:00', '17:00'),
+        ('18:00', '18:00'),
+        ('19:00', '19:00'),
+        ('20:00', '20:00'),
+        ('21:00', '21:00'),
+    )
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.IntegerField()
     requirements = models.TextField(blank=True, null=True)
-    number_of_guests = models.IntegerField()
+    table_size = models.ForeignKey(Tables, null=True, on_delete=models.SET_DEFAULT, default='2')
     date = models.DateField()
     time = models.CharField(max_length=5, choices=TIME_CHOICES, default='12:00')
 
